d6f23db6c3089c10fba5be1546402bc4815c97d0
Eva Liu <evaliu2046@gmail.com>
2021-05-26 21:28:30 -0400

    Add Entity init files

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Employee.java
@@ -0,0 +1,62 @@
+package com.udacity.jdnd.course3.critter.entity;
+
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+import org.hibernate.annotations.Nationalized;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.DayOfWeek;
+import java.util.Set;
+
+
+/**
+ * Create Entity for Employee
+ */
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Nationalized
+    private String name;
+    private Set<EmployeeSkill> skills;
+    private Set<DayOfWeek> daysAvailable;
+
+    public Employee(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<EmployeeSkill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Set<EmployeeSkill> skills) {
+        this.skills = skills;
+    }
+
+    public Set<DayOfWeek> getDaysAvailable() {
+        return daysAvailable;
+    }
+
+    public void setDaysAvailable(Set<DayOfWeek> daysAvailable) {
+        this.daysAvailable = daysAvailable;
+    }
+}
